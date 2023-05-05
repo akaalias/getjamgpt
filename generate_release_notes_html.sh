cbe88a78ddb6e163c5f287ecf7c22b748a70eb67
Alexis Rondeau <alexis.rondeau@gmail.com>
2023-05-05 11:37:17 +0200

    1.8.9

--- generate_release_notes_html.sh
+++ generate_release_notes_html.sh
@@ -6,7 +6,7 @@ APPBUNDLE="$APPNAME.app"
 VERSION=$(scout read -i $APPBUNDLE/Contents/Info.plist -f plist "CFBundleVersion")
 
 export DMGNAME="$APPNAME-$VERSION.dmg"
-export HTMLNAME="$DMGNAME.html"
+export HTMLNAME="$APPNAME-$VERSION.html"
 
 echo "Let's tell them why they should update!"
 export EDITOR=/opt/homebrew/bin/emacs
